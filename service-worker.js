9244ee8d3388086e8af4ab66e4ea4c5b9f0bdb69
ahobbyist-blip <srinath.subscribe@gmail.com>
2026-08-06 12:28:57 +0200

    ui updates

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'daily-dashboard-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
